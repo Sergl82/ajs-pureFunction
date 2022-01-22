74bfa6b360c86b74a29a6c44f6038a70318b36f1
Sergey <84701382+Sergl82@users.noreply.github.com>
2022-01-22 22:32:23 +1100

    ajs-pureFunction

--- src/js/__tests__/health.test.js
+++ src/js/__tests__/health.test.js
@@ -1,17 +1,17 @@
 import healthString from '../health';
 
-  test('should return "healthy"', () => {
-    expect(healthString).toBeDefined();
-    const result = healthString({ name: 'Маг', health: 90 });
-    expect(result).toBe('healthy');
-  });
+test('should return "healthy"', () => {
+  expect(healthString).toBeDefined();
+  const result = healthString({ name: 'Маг', health: 90 });
+  expect(result).toBe('healthy');
+});
 
-  test('should return "wounded"', () => {
-    const result = healthString({ name: 'Стенобой', health: 30 });
-    expect(result).toBe('wounded');
-  });
+test('should return "wounded"', () => {
+  const result = healthString({ name: 'Стенобой', health: 30 });
+  expect(result).toBe('wounded');
+});
 
-  test('should return "critical"', () => {
-    const result = healthString({ name: 'Дракон', health: 3 });
-    expect(result).toBe('critical');
-  });
+test('should return "critical"', () => {
+  const result = healthString({ name: 'Дракон', health: 3 });
+  expect(result).toBe('critical');
+});
